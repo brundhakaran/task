42fa189e25870e8f079bb9218799999c712292be
brundha <brundhakarunakaran2201@gmail.com>
2025-06-30 01:11:37 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Task';
+  
+   toggleMenu: boolean = false;
+  showMenu: boolean= false;
+aboutMenu:boolean = false;
+servicesMenu:boolean = false;
+pagesMenu:boolean = false;
+blogMenu:boolean = false;
+ContactMenu:boolean = false;
+
+
+
+
+  onToggleMenu() {
+    this.toggleMenu = !this.toggleMenu;
+  }
+  
+
+  
+}
+ 
